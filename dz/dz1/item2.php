dcd0389db23acab38e8584181d8b5d19ffffb9ab
Roman Usov <usovrv@gmail.com>
2021-11-20 15:41:58 +0300

    Убрал двойные ковычки

--- dz/dz1/item2.php
+++ dz/dz1/item2.php
@@ -6,7 +6,7 @@ $byPaints = $totalDrawings - $drawingMarkers - $drawingPencils;
 $drawingPaints = $totalDrawings - $drawingMarkers - $drawingPencils;
 echo '<b>Задача:</b><br>';
 echo "На школьной выставке $totalDrawings рисунков. $drawingMarkers из них выполнены фломастерами, $drawingPencils карандашами, а остальные - красками.<br>";
-echo "Сколько рисунков, выполненных красками, на школьной выставке?<br>";
+echo 'Сколько рисунков, выполненных красками, на школьной выставке?<br>';
 echo '<b>Решение:</b><br>';
 echo 'Количество рисунков красками =<br>';
 echo "Общее количество рисунков ($totalDrawings) - количество рисунков фломастерами ($drawingMarkers) - количество рисунков карандашами ($drawingPencils)<br>";
